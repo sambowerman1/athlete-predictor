2786f93451c7e7f167fe9aba222ad85296cc6499
Sam Bowerman <sam@bowerman.org>
2024-03-20 18:51:55 -0500

    Added contact

--- server.js
+++ server.js
@@ -110,6 +110,10 @@ app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/public/about.html'); 
 });
 
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/public/contact.html'); 
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
